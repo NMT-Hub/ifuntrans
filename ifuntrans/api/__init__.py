60eeed2777045515f4ce0de566c7e1910da57360
BrightXiaoHan <beatmight@gmail.com>
2023-07-31 16:12:12 +0800

    finish translate docs

--- ifuntrans/api/__init__.py
+++ ifuntrans/api/__init__.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from pydantic import BaseModel
 
 from ifuntrans.metadata import (__version__, contact, description,
                                 license_info, title)
+
+
+class IfunTransModel(BaseModel):
+    @classmethod
+    def get_examples(cls):
+        return cls.model_config["json_schema_extra"]["examples"]
+
+    @classmethod
+    def get_example(cls):
+        return cls.get_examples()[0]
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [],
+        },
+    }
 
 
 def home():
@@ -20,5 +37,16 @@ def create_app():
         contact=contact,
         license_info=license_info,
     )
+
+    import ifuntrans.api.translate as translate
+
     app.get("/", summary="show SwaggerUI (this page)")(home)
+    app.post(
+        "/translate",
+        response_model=translate.TranslationResponse,
+        summary="translate text",
+        responses={200: translate.TranslationResponse.get_example()},
+    )(translate.translate)
+    app.get("/translate/supported_languages", summary="get supported languages")(translate.get_supported_languages)
+    app.get("/translate/avaliable_engines", summary="get avaliable engines")(translate.get_avaliable_engines)
     return app
--- ifuntrans/api/translate.py
+++ ifuntrans/api/translate.py
@@ -1,20 +1,45 @@
-from typing import Annotated
+from typing import Annotated, List
 
 import fastapi
-from pydantic import BaseModel
 
-from ifuntrans.api import app
+from ifuntrans.api import IfunTransModel
 
 __all__ = ["translate", "get_avaliable_engines", "get_avaliable_languages"]
 
 
-class TranslationRequest(BaseModel):
+SUPPORTED_ENGINES = ["google", "deepl", "bing", "yandex", "baidu", "tencent", "chatgpt"]
+
+
+class TranslationRequest(IfunTransModel):
     """model for a base request that require a source & target language and a text to translate"""
 
     engine: str = "google"
     source: str = "auto"
     target: str
     text: str
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "engine": "google",
+                    "source": "auto",
+                    "target": "en",
+                    "text": "Bonjour le monde",
+                }
+            ]
+        }
+    }
+
+
+class TranslationResponse(IfunTransModel):
+    """model for a base response that contain a translated text"""
+
+    translation: str
+    source: str
+    target: str
+
+    model_config = {"json_schema_extra": {"examples": [{"translation": "Hello world", "source": "fr", "target": "en"}]}}
 
 
 def translate(
@@ -23,17 +48,19 @@ def translate(
         fastapi.Body(
             ...,
             embed=True,
-            examples=[{"engine": "google", "source": "auto", "target": "en", "text": "Bonjour le monde"}],
+            examples=TranslationRequest.get_examples(),
         ),
     ]
-):
+) -> TranslationResponse:
     pass
 
 
-def get_avaliable_engines():
-    return ["google", "deepl", "bing"]
+def get_avaliable_engines(
+    source: Annotated[str, fastapi.Query(..., description="source language", example="fr")],
+    target: Annotated[str, fastapi.Query(..., description="target language", example="en")],
+) -> List[str]:
+    return SUPPORTED_ENGINES
 
 
-@app.route("/get_supported_languages", methods=["GET"])
-def get_supported_languages():
-    return "Hello, World!"
+def get_supported_languages(engine: str = "google"):
+    return {"languages": ["en", "zh", "fr", "de", "ja", "ko", "es", "pt", "it", "ru", "vi", "id", "th", "ms", "ar"]}
