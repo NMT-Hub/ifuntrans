f2d761febaeb623a28ba8c28d48e3500bcc2466e
BrightXiaoHan <beatmight@gmail.com>
2023-08-07 18:02:27 +0800

    update

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,15 +1,21 @@
 import pytest
+from fastapi.testclient import TestClient
 
-from ifuntrans.api import create_app
 import ifuntrans.api.translate as translate
+from ifuntrans.api import create_app
 
 
-@pytest.fixture(scope="module", autouse=True))
-def app():
+@pytest.fixture(scope="module", autouse=True)
+def client():
     app = create_app()
-    yield app
+    yield TestClient(app)
 
 
-def test_translate(app):
+def test_translate(client):
     request = translate.TranslationRequest.get_example()
+    response = client.post("/translate", json=request).json()
+    translate.TranslationResponse(**response)
 
+
+def test_localization(client):
+    pass
