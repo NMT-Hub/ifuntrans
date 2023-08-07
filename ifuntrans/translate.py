30215a4e17df00817ee6e3c440fa073854c888ce
BrightXiaoHan <beatmight@gmail.com>
2023-08-07 16:02:05 +0800

    update

--- ifuntrans/translate.py
+++ ifuntrans/translate.py
@@ -3,15 +3,33 @@ Translate all in one.
 """
 from typing import List
 
+import langcodes
+
 from ifuntrans.translators import GoogleTranslator
 
 
-def translate(data: List[str], from_lang: str, to_lang: str, prompt="") -> List[str]:
+def translate(texts: List[str], from_lang: str, to_lang: str, prompt="") -> List[str]:
     """
     Translate the given dataframe to the given languages.
-    :param df: The dataframe to translate.
+    :param texts: The texts to translate.
+    :param prompt: The prompt to show.
     :param to_langs: The languages to translate to.
     :return: The translated dataframe.
     """
+    need_translate_mask = []
+    if langcodes.closest_supported_match(from_lang, ["en"]) is not None:
+        need_translate_mask = [True] * len(texts)
+
+    else:
+        # if only contains ascii characters and number of word is less than 2, then don't translate
+        for text in texts:
+            if text.isascii() and len(text.split()) <= 2:
+                need_translate_mask.append(False)
+            else:
+                need_translate_mask.append(True)
+
     translator = GoogleTranslator(from_lang, to_lang)
-    return translator.translate_batch(data)
+    translation = translator.translate_batch(texts)
+
+    translation = [t if need_translate_mask[i] else texts[i] for i, t in enumerate(translation)]
+    return translation
--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import os
 
 import ifuntrans.api.localization as localization
 
-test_file = os.path.join(os.path.dirname(__file__), "assets/small.xlsx")
+test_file = os.path.join(os.path.dirname(__file__), "tests/assets/small.xlsx")
 
 
-def test_translate():
+def main():
     # 翻译到英语，中文繁体（台繁），印尼语，越南语，泰语，巴西葡萄牙语，日，韩，阿拉伯，土耳其
     output_file = "~/Desktop/translated.xlsx"
     localization.translate_excel(test_file, output_file, "en,zh-TW,id,vi,th,pt-BR,ja,ko,ar,tr")
-    assert os.path.exists(output_file)
+
+
+if __name__ == "__main__":
+    main()
