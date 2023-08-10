26c2fecc67d7ff0c29fa8a08c137aaddf0a54cd2
BrightXiaoHan <beatmight@gmail.com>
2023-08-10 10:24:25 +0800

    fix source.ps1

--- scripts/source.ps1
+++ scripts/source.ps1
@@ -2,10 +2,12 @@
 $envFile = Get-Content -Path .\.env
 
 foreach ($line in $envFile) {
-    if ($line -match '^export (.*?)=(.*)$') {
+    if ($line -match '^(.*?)=(.*)$') {
         $key = $matches[1]
         $value = $matches[2]
         # remove quotes
+        $value = $value -replace "^\'|\'$"
+        # remove double quotes
         $value = $value -replace '^"|"$'
         Set-Item -Path "env:$key" -Value $value
         echo "Setting $key to $value"
