07721ef5db1159db2b0aef938fabd9f18fcf3e37
BrightXiaoHan <beatmight@gmail.com>
2023-08-10 16:34:07 +0800

    update

--- ifuntrans/translators/base.py
+++ ifuntrans/translators/base.py
@@ -92,7 +92,6 @@ class BaseTranslator(ABC):
                             f"{self._languages}",
                         )
 
-                import ipdb; ipdb.set_trace()
                 language = langcodes.closest_supported_match(language, self._languages.values())
                 if not language:
                     raise LanguageNotSupportedException(
