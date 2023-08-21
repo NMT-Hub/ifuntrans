e4ade84df6ac082f189ca034667cd145b3e5017d
BrightXiaoHan <beatmight@gmail.com>
2023-08-21 11:18:38 +0800

    使用规则修复翻译句式的一致性

--- scripts/pe.py
+++ scripts/pe.py
@@ -2,18 +2,13 @@
 
 import argparse
 import re
-from itertools import chain
 
 import langcodes
 import pandas
 
-from ifuntrans.pe import chatgpt_doc_translate, hardcode_post_edit, tokenizer
+from ifuntrans.pe import hardcode_post_edit, tokenizer
 
-
-def _id_group_func(row):
-    # replace all number by blank
-    row["ID"] = re.sub(r"\d+", "", row["ID"])
-    return row
+RE_NORMALIZED_NUMBER = r"-?\d{1,3}(?:,\d{3})*"
 
 
 def _prefix_group_func(row):
@@ -32,15 +27,22 @@ def _suffix_group_func(row):
     return row
 
 
+def _duplicate_group_func(row):
+    string = row.iloc[1]
+    string = re.sub(RE_NORMALIZED_NUMBER, "xxx", string)
+    row.iloc[0] = string
+    return row
+
+
 def pe(input_file, output_file):
     # Read input file
     df = pandas.read_excel(input_file, sheet_name=None)
     main_sheet = df["Translation Summary"]
 
     for sheet_name, sheet_df in df.items():
-        # if sheet_name != "Thai":
-        #     continue
         if sheet_name == "Translation Summary":
+            continue
+        if "Taiwan" in sheet_name:
             continue
         columns = sheet_df.columns
         src_lang_name = columns[1]
@@ -49,25 +51,62 @@ def pe(input_file, output_file):
 
         results = hardcode_post_edit(sheet_df[columns[1]].tolist(), sheet_df[columns[-1]].tolist(), src_lang, tgt_lang)
         sheet_df["Hardcode PE"] = results
+        main_sheet[sheet_name].update(sheet_df["Hardcode PE"])
 
-        # group by id
-        sheet_df["ChatGPT Fix Consistency"] = None
-        group_prefix = sheet_df.apply(_prefix_group_func, axis=1).groupby(columns[0])
-        group_suffix = sheet_df.apply(_suffix_group_func, axis=1).groupby(columns[0])
-        for _, group_df in chain(group_prefix, group_suffix):
-            if group_df.shape[0] < 10:
+        # # group by id
+        # sheet_df["ChatGPT Fix Consistency"] = None
+        # group_prefix = sheet_df.apply(_prefix_group_func, axis=1).groupby(columns[0])
+        # group_suffix = sheet_df.apply(_suffix_group_func, axis=1).groupby(columns[0])
+        # for _, group_df in chain(group_prefix, group_suffix):
+        #     if group_df.shape[0] < 10:
+        #         continue
+
+        #     prev_result = group_df[columns[-1]].tolist()
+        #     group_results = chatgpt_doc_translate(
+        #         group_df[columns[1]].tolist(), prev_result, src_lang, tgt_lang
+        #     )
+
+        #     group_df["ChatGPT Fix Consistency"] = group_results
+        #     sheet_df.update(group_df, overwrite=False)
+
+        # main_sheet[sheet_name].update(sheet_df["ChatGPT Fix Consistency"])
+
+        # group by duplicate
+        sheet_df["HardCoding Fix Consistency"] = None
+        group_duplicate = sheet_df.apply(_duplicate_group_func, axis=1).groupby(columns[0])
+        for _, group_df in group_duplicate:
+            if group_df.shape[0] < 2:
                 continue
-            
-            prev_result = group_df[columns[-1]].tolist()
-            group_results = chatgpt_doc_translate(
-                group_df[columns[1]].tolist(), prev_result, src_lang, tgt_lang
-            )
+            template = group_df.iloc[0]
+            template_tgt = template[-2]
 
-            group_df["ChatGPT Fix Consistency"] = group_results
+            fixed = []
+            for _, row in group_df.iterrows():
+                src = row[1]
+                all_nums = re.findall(RE_NORMALIZED_NUMBER, src)
+
+                i = 0
+
+                def _duplicate_replace_func(obj):
+                    nonlocal i
+                    i += 1
+                    try:
+                        num = all_nums[i - 1]
+                    except IndexError:
+                        num = obj.group(0)
+                    try:
+                        num = int(num)
+                        num = f"{num:,}"
+                    except ValueError:
+                        pass
+                    return num
+
+                fixed.append(re.sub(RE_NORMALIZED_NUMBER, _duplicate_replace_func, template_tgt))
+
+            group_df["HardCoding Fix Consistency"] = fixed
             sheet_df.update(group_df, overwrite=False)
 
-        main_sheet[sheet_name].update(sheet_df["ChatGPT Fix Consistency"])
-        # break
+        main_sheet[sheet_name].update(sheet_df["HardCoding Fix Consistency"])
 
     # Write output file
     with pandas.ExcelWriter(output_file, engine="openpyxl") as writer:
