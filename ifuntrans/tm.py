09573610f54caefaeed4438229f36fbd25e2c18c
BrightXiaoHan <beatmight@gmail.com>
2023-08-28 13:49:01 +0800

    完成了whoosh搜索记忆库的功能

--- ifuntrans/tm.py
+++ ifuntrans/tm.py
@@ -1,21 +1,20 @@
 """Translation Memory"""
-import os
 import pathlib
-import shutil
 from functools import cache
-from typing import Annotated, List
+from typing import Tuple
 
-import fastapi
-import langcodes
 import pandas
 from whoosh.fields import ID, TEXT, Schema
-from whoosh.index import create_in, open_dir
+from whoosh.filedb.filestore import RamStorage
 from whoosh.qparser import OrGroup, QueryParser
+
+from ifuntrans.async_translators.chatgpt import tokenizer
 
 
 @cache
 def get_index_path():
-    return (pathlib.Path(__file__).parent.parent / ".index").resolve().as_posix()
+    # return (pathlib.Path(__file__).parent.parent / ".index").resolve().as_posix()
+    return ":memory:"
 
 
 @cache
@@ -23,70 +22,58 @@ def get_tm_path():
     return (pathlib.Path(__file__).parent.parent / "assets" / "tm.xlsx").resolve().as_posix()
 
 
-def init_tm_indexing(force_reload=False):
+def tokenize(string: str) -> str:
+    tokens = tokenizer.encode(string)
+    return " ".join([str(token) for token in tokens])
+
+
+def detokenize(string: str) -> str:
+    tokens = string.split(" ")
+    tokens = [int(token) for token in tokens]
+    return tokenizer.decode(tokens)
+
+
+def init_tm_indexing():
     global IX
-    if os.path.exists(get_index_path()) and not force_reload:
-        IX = open_dir(get_index_path())
-        return IX
-
-    if force_reload:
-        shutil.rmtree(get_index_path(), ignore_errors=True)
-
     tm_df = pandas.read_excel(get_tm_path())
 
     # skip first two rows
     tm_df = tm_df.iloc[1:]
 
     langs = tm_df.columns[1:].tolist()
-
-    columns = {}
-    for lang in langs:
-        try:
-            lang_code = langcodes.find_name("language", lang, "zh")
-        except:
-            continue
-        columns[lang_code.language] = TEXT(stored=True)
-
-    os.makedirs(os.path.dirname(get_index_path()), exist_ok=True)
+    columns = {lang: TEXT(stored=True) for lang in langs}
     schema = Schema(
-        id=ID(stored=True),
+        STR_ID=ID(stored=True),
         **columns,
     )
-    os.mkdir(get_index_path())
-    IX = create_in(
-        get_index_path(),
+
+    st = RamStorage()
+    IX = st.create_index(
         schema,
     )
 
     writer = IX.writer()
     for _, row in tm_df.iterrows():
-        import ipdb; ipdb.set_trace()
+        docs = {}
+        for lang in langs:
+            string = str(row[lang])
+            docs[lang] = tokenize(string)
+
+        # tokenized data
         writer.add_document(
-            id=str(row.iloc[0]),
-            **{lang_code: row[lang] for lang, lang_code in columns.items()},
+            STR_ID=str(row.iloc[0]),
+            **docs,
         )
-
     writer.commit()
 
 
-def delete_semantic_words(
-    group_ids: Annotated[
-        List[str],
-        fastapi.Body(..., embed=True, example=["1677149648288296961", "1677149648288296962"]),
-    ],
-):
-    writer = IX.writer()
-    for group_id in group_ids:
-        writer.delete_by_term("group_id", group_id)
-    writer.commit()
-
-
-def match_semantic_words(
-    context: Annotated[str, fastapi.Body(..., embed=True, example="我要投诉")],
-):
+def search_tm(text: str, source_lang: str, target_lang: str, limit=1) -> Tuple[str, str]:
     with IX.searcher() as searcher:
-        # query = jieba.lcut(context)
-        query = " OR ".join(query)
-        parser = QueryParser("content", IX.schema, group=OrGroup)
-        query = parser.parse(query)
-        results = searcher.search(query, limit=10)
+        tokens = tokenize(text)
+        query = QueryParser(source_lang, IX.schema, group=OrGroup).parse(tokens)
+        results = searcher.search(query, limit=limit)
+        if len(results) == 0:
+            return "", ""
+        searched_source = detokenize(results[0][source_lang])
+        searched_target = detokenize(results[0][target_lang])
+        return searched_source, searched_target
--- tests/test_tm.py
+++ tests/test_tm.py
@@ -1,9 +1,21 @@
-import tempfile
-from ifuntrans.tm import init_tm_indexing
+import pandas
+
+from ifuntrans.tm import get_tm_path, init_tm_indexing, search_tm
 
 
-def test_tm(mocker):
+def test_tm():
+    init_tm_indexing()
+    # search
+    tm_path = get_tm_path()
+    tm_df = pandas.read_excel(tm_path)
+    tm_df = tm_df.iloc[1:]
 
-    with tempfile.TemporaryDirectory() as temp:
-        mocker.patch("ifuntrans.tm.get_index_path", return_value=temp)
-        init_tm_indexing(force_reload=True)
+    # random choose some rows
+    rows = tm_df.sample(10).iterrows()
+
+    for _, row in rows:
+        source_lang = row.index[1]
+        for target_lang in tm_df.columns[2:]:
+            string = str(row[source_lang])
+            source, _ = search_tm(string, source_lang, target_lang)
+            assert source in string
